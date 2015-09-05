3b67111787e797e58a22a9824a7d710f4644707c
Pedro Visintin <pedro.visintin@gmail.com>
2015-09-05 22:01:34 +0200

    only start the app if the Space.isReady

--- boss.js
+++ boss.js
@@ -9,34 +9,50 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var entrance = Space.addCheckpoint({slug: 'entrance', device_path: 'USB_08ff_0009_14541300', position: 1});
-var mid = Space.addCheckpoint({slug: 'mid', device_path: 'USB_08ff_0009_14541400', position: 2});
-var last = Space.addCheckpoint({slug: 'last', device_path: 'USB_08ff_0009_14541200', position: 3});
+/*
+As the devicePath change every time this should be reconfigured everytime, maybe a config file and a setup cli command will be a good idea
+*/
 
+var entrance = Space.addCheckpoint({slug: 'entrance', devicePath: 'USB_08ff_0009_14541000', position: 1});
+var mid = Space.addCheckpoint({slug: 'mid', devicePath: 'USB_08ff_0009_14541400', position: 2});
+var last = Space.addCheckpoint({slug: 'last', devicePath: 'USB_08ff_0009_14541200', position: 3});
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.emit('chat message', 'I\'m tracking you...' );
-  entrance.reader.on('read', function(data){ 
-    Checking.read(data.checkpoint, data.id);
-    io.emit('chat message', data.checkpoint.position + ':' + data.id );
+if (Space.isReady()) {
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
   });
-  mid.reader.on('read', function(data){ 
-    Checking.read(data.checkpoint, data.id);
-    io.emit('chat message', data.checkpoint.position + ':' + data.id );
+
+  io.on('connection', function(socket){
+    socket.emit('chat message', 'I\'m tracking you...' );
+    entrance.reader.on('read', function(data){ 
+      Checking.read(data.checkpoint, data.id);
+      io.emit('chat message', data.checkpoint.position + ':' + data.id );
+    });
+    mid.reader.on('read', function(data){ 
+      Checking.read(data.checkpoint, data.id);
+      io.emit('chat message', data.checkpoint.position + ':' + data.id );
+    });
+    last.reader.on('read', function(data){ 
+      Checking.read(data.checkpoint, data.id);
+      io.emit('chat message', data.checkpoint.position + ':' + data.id );
+    });
   });
-  last.reader.on('read', function(data){ 
-    Checking.read(data.checkpoint, data.id);
-    io.emit('chat message', data.checkpoint.position + ':' + data.id );
-  });
-});
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+  
+} else {
+  console.log('Your boss is having holidays...');
+  Space._checkpoints.filter(
+    function(element, index){
+      return element.up;
+    }).forEach( 
+      function(element, index) {
+        console.log('Device ' + element.devicePath + ' not found');
+    });
+}
+
 
 
