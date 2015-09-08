3d7f5b00d78fcd325ed131e32ef7c20c963d9941
Pedro Visintin <pedro.visintin@gmail.com>
2015-09-08 16:46:35 +0200

    change date type to avoid timezone problems

--- models/visitortrack.js
+++ models/visitortrack.js
@@ -1,10 +1,21 @@
 'use strict';
 
+var Sequelize = require('sequelize');
+var moment = require('moment');
+
 module.exports = function(sequelize, DataTypes) {
   var VisitorTrack = sequelize.define('VisitorTrack', {
     cardId: DataTypes.STRING,
     position: DataTypes.INTEGER,
+    createdAt: {
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      defaultValue: moment().utc().toDate()
+    }
   }, {
+    timestamps: false,
     classMethods: {
       associate: function(models) {
         // associations can be defined here
