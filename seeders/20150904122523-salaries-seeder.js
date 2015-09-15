88ee9c87f93c07eab2bd8e9045f516ce65f0e7a0
Pedro Visintin <pedro.visintin@gmail.com>
2015-09-15 21:27:30 +0200

    update salaries seeder with underscores

--- seeders/20150904122523-salaries-seeder.js
+++ seeders/20150904122523-salaries-seeder.js
@@ -5,19 +5,19 @@ module.exports = {
 
     var now = new Date();
 
-    return queryInterface.bulkInsert('Salaries', [
-      { monthlyIncome: 830000, text: "Philip Lahm", createdAt: now, updatedAt: now },
-      { monthlyIncome: 16000,  text: "Chief physician", createdAt: now, updatedAt: now },
-      { monthlyIncome: 11100,  text: "Manager", createdAt: now, updatedAt: now },
-      { monthlyIncome: 10300,  text: "Pilot", createdAt: now, updatedAt: now },
-      { monthlyIncome: 6100,   text: "Judge", createdAt: now, updatedAt: now },
-      { monthlyIncome: 4030,   text: "School teacher", createdAt: now, updatedAt: now },
-      { monthlyIncome: 2830,   text: "Car mechanic", createdAt: now, updatedAt: now },
-      { monthlyIncome: 2576,   text: "Insurance salesman", createdAt: now, updatedAt: now },
-      { monthlyIncome: 2064,   text: "Nurse", createdAt: now, updatedAt: now },
-      { monthlyIncome: 1288,   text: "Hairdresser", createdAt: now, updatedAt: now },
-      { monthlyIncome: 670,    text: "Student", createdAt: now, updatedAt: now }
-    ], { silent: true }, ['monthlyIncome', 'text']);
+    return queryInterface.bulkInsert('salaries', [
+      { monthly_income: 830000, text: "Philip Lahm", updated_at : now, created_at: now },
+      { monthly_income: 16000,  text: "Chief physician", updated_at: now, created_at: now },
+      { monthly_income: 11100,  text: "Manager", updated_at: now, created_at: now },
+      { monthly_income: 10300,  text: "Pilot", updated_at: now, created_at: now },
+      { monthly_income: 6100,   text: "Judge", updated_at: now, created_at: now },
+      { monthly_income: 4030,   text: "School teacher", updated_at: now, created_at: now },
+      { monthly_income: 2830,   text: "Car mechanic", updated_at: now, created_at: now },
+      { monthly_income: 2576,   text: "Insurance salesman", updated_at: now, created_at: now },
+      { monthly_income: 2064,   text: "Nurse", updated_at: now, created_at: now },
+      { monthly_income: 1288,   text: "Hairdresser", updated_at: now, created_at: now },
+      { monthly_income: 670,    text: "Student", updated_at: now, created_at: now }
+    ], { silent: true }, ['monthly_income', 'text']);
 
 
   },
